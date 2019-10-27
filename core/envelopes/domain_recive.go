c8a6dc7560b8da0edb0516358cc5d4b864e099a8
solozyx <zhaoyingxin@aliyun.com>
2019-10-27 18:21:00 +0800

    收红包

--- core/envelopes/domain_recive.go
+++ core/envelopes/domain_recive.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+
 	"github.com/shopspring/decimal"
 	"github.com/tietang/dbx"
 
@@ -44,11 +46,13 @@ func (domain *goodsDomain) Receive(ctx context.Context, dto services.RedEnvelope
 		domain.itemDomain.RedEnvelopeItem.Quantity = 1
 		domain.itemDomain.RedEnvelopeItem.PayStatus = int(services.Paying)
 		domain.itemDomain.RedEnvelopeItem.AccountNo = dto.AccountNo
+		// 本次抢到红包后的红包剩余金额
 		domain.itemDomain.RedEnvelopeItem.RemainAmount = goods.RemainAmount.Sub(nextAmount)
 		// 本次抢到的红包金额
 		domain.itemDomain.RedEnvelopeItem.Amount = nextAmount
 		// 构造新的上下文 传入 *TxRunner 数据库事务对象
 		txCtx := base.WithValueContext(ctx, runner)
+		// 写入 red_envelope_item 表
 		_, err = domain.itemDomain.Save(txCtx)
 		if err != nil {
 			return err
@@ -65,23 +69,27 @@ func (domain *goodsDomain) Receive(ctx context.Context, dto services.RedEnvelope
 
 // 预创建收红包订单明细
 func (domain *goodsDomain) preCreateItem(dto services.RedEnvelopeReceiveDTO) {
+	// 收红包 和 账户 关联
 	domain.itemDomain.RedEnvelopeItem.AccountNo = dto.AccountNo
+	// // 收红包 和 发出的红包编号 关联
 	domain.itemDomain.RedEnvelopeItem.EnvelopeNo = dto.EnvelopeNo
 	domain.itemDomain.RedEnvelopeItem.RecvUsername = sql.NullString{
 		String: dto.RecvUsername,
 		Valid:  true,
 	}
 	domain.itemDomain.RedEnvelopeItem.RecvUserId = dto.RecvUserId
-	domain.itemDomain.createItemNo()
 
-	//goods := domain.Get(dto.EnvelopeNo)
-	//var s string
-	//if goods.EnvelopeType == int(services.GeneralEnvelopeType) {
-	//	s = "普通"
-	//} else {
-	//	s = "碰运气"
-	//}
-	//domain.itemDomain.Desc = fmt.Sprintf("%s的%s红包", goods.Username.String, s)
+	envelopeGoods := domain.Get(dto.EnvelopeNo)
+	var s string
+	if envelopeGoods.EnvelopeType == int(services.GeneralEnvelopeType) {
+		s = "普通"
+	} else {
+		s = "碰运气"
+	}
+	domain.itemDomain.RedEnvelopeItem.Desc = fmt.Sprintf("%s的%s红包",
+		envelopeGoods.Username.String, s)
+
+	domain.itemDomain.createItemNo()
 }
 
 // 计算红包金额
@@ -117,7 +125,9 @@ func (domain *goodsDomain) transfer(ctx context.Context, dto services.RedEnvelop
 		Username:  dto.RecvUsername,
 	}
 	if target.AccountNo == "" {
-		a := accounts.NewAccountDomain().GetAccountByUserIdAndType(target.UserId, services.EnvelopeAccountType)
+		a := accounts.
+			NewAccountDomain().
+			GetAccountByUserIdAndType(target.UserId, services.EnvelopeAccountType)
 		target.AccountNo = a.AccountNo
 	}
 	transferDTO := services.AccountTransferDTO{
@@ -125,24 +135,12 @@ func (domain *goodsDomain) transfer(ctx context.Context, dto services.RedEnvelop
 		TradeBody:   body,
 		TradeTarget: target,
 		// 本次抢到红包金额
-		Amount: domain.itemDomain.RedEnvelopeItem.Amount,
-		// TODO ???
-		ChangeType: services.EnvelopeIncoming,
-		ChangeFlag: services.FlagTransferIn,
-		Desc:       "红包收入",
+		Amount:     domain.itemDomain.RedEnvelopeItem.Amount,
+		ChangeType: services.EnvelopeOutgoing,
+		ChangeFlag: services.FlagTransferOut,
+		Desc:       "系统红包账户扣减,转入收红包账户,红包编号 " + dto.EnvelopeNo,
 	}
 	accountDomain := accounts.NewAccountDomain()
 	// 系统账户扣减资金
 	return accountDomain.TransferWithContextTx(ctx, transferDTO)
-
-	// 用户账户增加资金
-	//transferDTO = services.AccountTransferDTO{
-	//	TradeBody:   target,
-	//	TradeTarget: body,
-	//	Amount:      domain.itemDomain.Amount,
-	//	ChangeType:  services.EnvelopeIncoming,
-	//	ChangeFlag:  services.FlagTransferIn,
-	//	Desc:        "抢红包成功收入",
-	//}
-	//return accountDomain.TransferWithContextTx(ctx, transferDTO)
 }
